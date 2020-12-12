d14b0ff926a287adec0105ea3191dff7db5b7845
ACsDave <davidlive00@hotmail.com>
2020-12-11 22:18:44 -0600

    Practica 3

---
+++ Hash_values.ps1
@@ -0,0 +1,20 @@
+﻿function verify-path{
+
+    param([Parameter(Mandatory, ValueFromPipeline)] [string] $path, [string]$HashFile= "Hash_file.txt")
+
+    process{
+        
+        $ruta= Test-Path -Path $path
+
+        if ($ruta -eq $true){
+            Get-ChildItem $path | Get-FileHash | Select-Object -Property Hash, Path | Format-Table -HideTableHeaders | Out-File $HashFile -Encoding ascii -ErrorAction SilentlyContinue
+            Write-Host "Archivo creado con éxito (:"
+        }else{
+
+            Write-Host "La ruta no existe"
+        }
+        
+    }
+}
+$fold= Read-Host "Ingresa el path: "
+$fold | verify-path
